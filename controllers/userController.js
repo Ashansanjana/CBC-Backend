666b8498b48f453803c39dae74acea0760015f4c
Ashansanjana <ashansanjana03@gmail.com>
2025-08-24 16:20:32 +0530

    i created new backend express js with mongoDB.created user login ,user create and jwt authentication

---
+++ controllers/userController.js
@@ -0,0 +1,77 @@
+import User from "../models/user.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
+export function createUser(req, res) {
+    if(req.body.role="admin" ){
+        if(req.user!=null){
+            if(req.user.role!="admin"){
+                return res.status(403).json({
+                    message:"You are not authorized to create an admin user"
+                })
+                return;
+            }
+
+        }else{
+            res.status(403).json({
+                message:"You are not authorized to create an admin user"
+            })
+            return;
+        }
+    }
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+    const user = new User({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: hashedPassword,
+        role: req.body.role,
+        isBlocked: req.body.isBlocked
+       
+    });
+
+    user.save().then(() => {
+        res.json({
+            message: 'User created successfully'
+        });
+    }).catch((error) => {
+        res.status(500).json({
+            message: 'Error creating user'
+        });
+    });
+}
+
+export function loginUser(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email: email }).then((user) => {
+        if (user==null) {
+            return res.status(404).json({ message: 'email is invalid' });
+        }else{
+                const isPasswordValid = bcrypt.compareSync(password, user.password);
+                if (isPasswordValid) {
+                  //create a token  
+                    const token = jwt.sign(
+                        {   email: user.email,
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            role: user.role,
+                            img : user.img   
+                        } ,
+                        "ashan@1117"
+                    )
+
+                    res.json({
+                        message : 'Login successful',
+                        token: token
+                    })}else {
+                    res.status(401).json({
+                        message: 'Password is invalid'
+                    });
+        }}})
+
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import userRouter from './routes/userRouter.js';
+import jwt from 'jsonwebtoken';
+
+const  app = express();
+
+mongoose.connect('mongodb+srv://admin:1234@cluster0.3x7vxfc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+    console.log('MongoDB connected successfully')}).catch((err) => {
+    console.error('MongoDB connection error:', err)});
+
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    const tokenString = req.header('Authorization');
+    if(tokenString!=null){
+        const token = tokenString.replace('Bearer ', '');
+        jwt.verify(token, 'ashan@1117', (err, decoded) => {
+        if(decoded!=null){
+            req.user = decoded;
+            next();
+        }
+        else{
+            res.status(403).json({ message: 'invalid token' });
+        }
+    
+    //next() 
+})}else
+{next()}}
+);
+
+
+app.use('/user', userRouter);
+
+
+
+app.listen(
+    3000, ()=>{
+        console.log('Server successfully started in port 3000');
+    }
+);
+
+
+//mongodb+srv://admin:1234@cluster0.3x7vxfc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
